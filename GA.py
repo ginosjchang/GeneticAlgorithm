e267f31e81044366f18d376b8649b27e7d77b6b3
ginosjchang <ginosjchang@gmail.com>
2022-10-06 10:28:28 +0800

    crossover and mutation are using function pointer for GeneticAlgorithm class

--- GA.py
+++ GA.py
@@ -14,17 +14,92 @@ class Problem:
             totalTime += self.input[task][agent]
         return totalTime
 
+def partial_mapped_crossover(chromosomes, population_size, crossover_size):
+    numTasks = len(chromosomes[0])
+
+    for offset in range(0, crossover_size, 2):
+        mapping = []
+        for i in range(numTasks):
+            mapping.append(-1)
+
+        p1 = random.randint(0, population_size)
+        p2 = random.randint(0, population_size)
+        c1 = population_size + offset
+        c2 = c1 + 1
+
+        rand1 = random.randint(0, numTasks - 2)
+        rand2 = random.randint(rand1 + 1, numTasks - 1)
+
+        for i in range(rand1, rand2 + 1):
+            c1_gene = chromosomes[p1][i]
+            c2_gene = chromosomes[p2][i]
+
+            if c1_gene == c2_gene: continue
+            elif mapping[c1_gene] == -1 and mapping[c2_gene] == -1:
+                mapping[c1_gene] = c2_gene
+                mapping[c2_gene] = c1_gene
+            elif mapping[c1_gene] == -1:
+                mapping[c1_gene] = mapping[c2_gene]
+                mapping[c2_gene] = -1
+                mapping[mapping[c1_gene]] = c1_gene
+            elif mapping[c2_gene] == -1:
+                mapping[mapping[c1_gene]] = c2_gene
+                mapping[c2_gene] = mapping[c1_gene]
+                mapping[c1_gene] = -1
+            else:
+                mapping[mapping[c1_gene]] = mapping[c2_gene]
+                mapping[mapping[c2_gene]] = mapping[c1_gene]
+                mapping[c1_gene] = -1
+                mapping[c2_gene] = -1
+
+        for i in range(numTasks):
+            if(i>=rand1 and i<=rand2):
+                chromosomes[c1][i] =  chromosomes[p2][i]
+                chromosomes[c2][i] =  chromosomes[p1][i]
+            else:
+                if(mapping[chromosomes[p1][i]] >=0):
+                    chromosomes[c1][i] = mapping[chromosomes[p1][i]]
+                else:
+                    chromosomes[c1][i] = chromosomes[p1][i]        
+                    
+                if(mapping[chromosomes[p2][i]] >=0):
+                    chromosomes[c2][i] = mapping[chromosomes[p2][i]]
+                else:
+                    chromosomes[c2][i] = chromosomes[p2][i]
+
+def invers_mutation(chromosomes, population_size, mutation_size):
+    numTasks = len(chromosomes[0])
+
+    for offset in range(mutation_size):
+        p = random.randint(0, population_size)
+        c = population_size + offset
+
+        rand1 = random.randint(0, numTasks-2)
+        rand2 = random.randint(rand1 + 1, numTasks - 1)
+        for i in range(numTasks):
+            if(i < rand1 or i > rand2):
+                chromosomes[c][i] = chromosomes[p][i]
+            else:
+                index = rand2 - (i - rand1)
+                chromosomes[c][i] = chromosomes[p][index]
+
 class GeneticAlgorithm:
-    def __init__(self, numTasks, cost, total_size):
+    def __init__(self, numTasks, cost, total_size, 
+                crossover_func = partial_mapped_crossover, mutation_func = invers_mutation,
+                population_rate = 0.7, crossover_rate = 0.2):
 
         self.total_size = total_size
-        self.population_size = int(total_size * 0.7)
-        self.crossover_size = int(total_size * 0.2)
+        self.population_size = int(total_size * population_rate)
+        self.crossover_size = int(total_size * crossover_rate)
         if self.crossover_size % 2 == 1:
             self.crossover_size -= 1
         self.mutation_size = self.total_size - self.population_size - self.crossover_size
+
         self.numTasks = numTasks
         self.cost = cost
+
+        self.crossover = crossover_func
+        self.mutation = mutation_func
         '''
         print("population_size: ", self.population_size)
         print("crossover_size: ", self.crossover_size)
@@ -49,94 +124,16 @@ class GeneticAlgorithm:
             self.chromosomes.append([])
             for j in range(self.numTasks):
                 self.chromosomes[i].append(-1)
-
-        #self.show_chromosomes()
-
-        for i in range(self.numTasks):
-            self.mapping.append(-1)
-
-        self.selected_chromosomes = []
-
-        self.best_chromosome = [0] * self.numTasks
-
-    def crossover(self):
-        child_index = [self.population_size, self.population_size + 1]
-        for i in range(int(self.crossover_size/2)):
-            self.__partial_mapped_crossover(random.randint(0, self.population_size), random.randint(0, self.population_size),
-                                            child_index[0], child_index[1])
-            child_index[0] += 2
-            child_index[1] += 2
-
-    def __partial_mapped_crossover(self, p1, p2, c1, c2):
-
-        for i in range(self.numTasks):
-            self.mapping[i] = -1
-
-        rand1 = random.randint(0, self.numTasks - 2)
-        rand2 = random.randint(rand1 + 1, self.numTasks - 1)
-
-        for i in range(rand1, rand2 + 1):
-            c1_gene = self.chromosomes[p1][i]
-            c2_gene = self.chromosomes[p2][i]
-
-            if c1_gene == c2_gene: continue
-            elif self.mapping[c1_gene] == -1 and self.mapping[c2_gene] == -1:
-                self.mapping[c1_gene] = c2_gene
-                self.mapping[c2_gene] = c1_gene
-            elif self.mapping[c1_gene] == -1:
-                self.mapping[c1_gene] = self.mapping[c2_gene]
-                self.mapping[c2_gene] = -1
-                self.mapping[self.mapping[c1_gene]] = c1_gene
-            elif self.mapping[c2_gene] == -1:
-                self.mapping[self.mapping[c1_gene]] = c2_gene
-                self.mapping[c2_gene] = self.mapping[c1_gene]
-                self.mapping[c1_gene] = -1
-            else:
-                self.mapping[self.mapping[c1_gene]] = self.mapping[c2_gene]
-                self.mapping[self.mapping[c2_gene]] = self.mapping[c1_gene]
-                self.mapping[c1_gene] = -1
-                self.mapping[c2_gene] = -1
-
-        for i in range(self.numTasks):
-            if(i>=rand1 and i<=rand2):
-                self.chromosomes[c1][i] =  self.chromosomes[p2][i]
-                self.chromosomes[c2][i] =  self.chromosomes[p1][i]
-            else:
-                if(self.mapping[self.chromosomes[p1][i]] >=0):
-                    self.chromosomes[c1][i] = self.mapping[self.chromosomes[p1][i]]
-                else:
-                    self.chromosomes[c1][i] =self.chromosomes[p1][i]        
-                
-                if(self.mapping[self.chromosomes[p2][i]] >=0):
-                    self.chromosomes[c2][i] = self.mapping[self.chromosomes[p2][i]]
-                else:
-                    self.chromosomes[c2][i] =self.chromosomes[p2][i]
-    
-    def mutation(self):
-        child_index = self.total_size - self.mutation_size - 1
-        for i in range(self.mutation_size):
-            parent_index = random.randint(0, self.population_size)
-            child_index += 1
-            self.invers_mutation(parent_index, child_index)
-
-    def invers_mutation(self, p, c):
-        rand1 = random.randint(0, self.numTasks-2)
-        rand2 = random.randint(rand1 + 1, self.numTasks - 1)
-        for i in range(self.numTasks):
-            if(i < rand1 or i > rand2):
-                self.chromosomes[c][i] = self.chromosomes[p][i]
-            else:
-                index = rand2 - (i - rand1)
-                self.chromosomes[c][i] = self.chromosomes[p][index]
     
     def sort_func(self, e):
         return e[1]
 
     def count_fitness(self):
         self.fitness.clear()
-        for i, chormosome in enumerate(self.chromosomes):
-            self.fitness.append([i, 1 / self.cost(chormosome)])
+        for i in range(self.total_size):
+            self.fitness.append([i, 1 / self.cost(self.chromosomes[i])])
 
+    def select_next_generation(self):
         self.fitness.sort(reverse = True ,key = self.sort_func)
         
         selected_chromosomes = []
@@ -146,12 +143,18 @@ class GeneticAlgorithm:
         
         for i in range(self.population_size):
             self.chromosomes[i] = selected_chromosomes[i]
-    
 
-    def select(self):
-        index = np.argsort(self.finess)[::-1]
     def get_best(self):
         return self.chromosomes[0]
+
+    def evolution(self, times = 100):
+        self.initialize()
+        for t in range(times):
+            self.crossover(self.chromosomes, self.population_size, self.crossover_size)
+            self.mutation(self.chromosomes, self.population_size + self.crossover_size, self.mutation_size)
+            self.count_fitness()
+            self.select_next_generation()
+    
     def show_chromosomes(self):
         print("Population")
         for i in range(self.population_size):
@@ -174,12 +177,8 @@ if __name__ == '__main__':
     solver = Problem(input)
     ga = GeneticAlgorithm(solver.numTasks, solver.cost, 50)
 
-    
     ga.initialize()
-    for i in range(50):
-        ga.crossover()
-        ga.mutation()
-        ga.count_fitness()
+    ga.evolution(times = 10)
     #ga.show_chromosomes()
 
     yourAssignment = ga.get_best()
